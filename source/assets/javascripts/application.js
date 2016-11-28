32f4dbec0f389876b94e53aa4fbe8ebefdc1679b
Matteo Scurati <matteo.scurati@gmail.com>
2016-11-28 18:41:48 +0100

    New font sizes

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,16 +1,10 @@
 import $ from 'jQuery';
-import flowtype from './vendor/flowtype.js';
 import lettering from './vendor/lettering.js';
 
 require('jquery-ui');
 require('modernizr');
 
 $(document).ready(() => {
-  $('.title').flowtype({
-    minFont   : 12,
-    maxFont   : 500,
-    fontRatio : 5
-  });
   $(".title h1").lettering();
 });
 
